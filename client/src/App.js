7e040813acb62ada3ebf84809c4c86963c9f750b
Frag782 <fragman782@gmail.com>
2024-02-01 12:15:09 -0500

    added server app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Subscribe from './components/Subscribe';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Login />*/}
+      {/*<Subscribe />*/}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,23 @@
+const Home = () => {
+
+    return (
+        <div>
+            <nav class='navbar bg-light navbar-expand-lg'>
+                <a class='navbar-brand' href='#'>Accueil</a>
+                <ul class='navbar-nav'>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='#'>Comptes</a>
+                    </li>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='#'>Profil</a>
+                    </li>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='#'>Se déconnecter</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Home;
